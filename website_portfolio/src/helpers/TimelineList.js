e873fdfc2c791a778c6fc28804f4c5efc3ccbbf3
HVerschueren <r0842391@student.thomasmore.be>
2022-12-31 00:02:04 +0100

    timeline gemaakt

---
+++ website_portfolio/src/helpers/TimelineList.js
@@ -0,0 +1,29 @@
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
+export const TimelineList = [
+    {
+        date: "2017-2020",
+        title: "Karel de Grote Hogeschool",
+        place: "Hoboken, Belgium",
+        explanation:"Bachelors degree in Multimedia and communications technology. Audio-Video",
+        icon:<SchoolIcon/>,
+        color:"rgb(19, 168, 158)",
+      },
+      {
+        date: "2020-now",
+        title: "started freelancing",
+        place: "Belgium, The Netherlands",
+        explanation:"EMG Netherlands, FlyAway, Mediapro",
+        icon:<WorkIcon/>,
+        color:"rgb(168,158,19)",
+      },
+      {
+        date: "2020-2023",
+        title: "Thomas More Hogeschool",
+        place: "Geel, Belgium",
+        explanation:"Bachelors degree in Elektronics-ICT. Digital Innovation/IoT",
+        icon:<SchoolIcon/>,
+        color:"rgb(19, 168, 158)",
+      },
+      
+];
