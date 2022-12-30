a6b4533ea08685b50f9bf1d0fc2f84bd39087a34
HVerschueren <r0842391@student.thomasmore.be>
2022-12-30 21:26:20 +0100

    footer met links

---
+++ website_portfolio/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='IconLinks'>
+            <a href='https://www.linkedin.com/in/hanne-verschueren-5683b2248/'><LinkedInIcon/></a>
+            <a href='https://github.com/HVerschueren'><GitHubIcon/></a>
+            <a href='mailto:testEmail.com'><EmailIcon/></a>
+        </div>
+        <p>&copy;2022 websiteURL</p>
+    </div>
+  )
+}
+
+export default Footer
