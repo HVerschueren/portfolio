5f8ef6817cba7c5d1f5f536e9f33be4b4a0828e0
HVerschueren <r0842391@student.thomasmore.be>
2022-12-30 21:12:11 +0100

    navbar

--- website_portfolio/src/App.js
+++ website_portfolio/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
   <div>
     <Router>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
