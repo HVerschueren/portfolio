82445196c9c5360269f937cc2814c8886eecda6a
HVerschueren <r0842391@student.thomasmore.be>
2022-12-30 20:44:52 +0100

    routes to home and projects

--- website_portfolio/src/App.js
+++ website_portfolio/src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 function App() {
-  return (<div></div>);
+  return (
+  <div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+  </div>
+  );
 }
 
 export default App;
