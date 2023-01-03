89a7c9859e02d8636734aa29809e877f04408e40
HVerschueren <r0842391@student.thomasmore.be>
2023-01-03 12:12:43 +0100

    timeLineElements zichtbaarder met shadow

--- website_portfolio/src/components/TimelineElement.js
+++ website_portfolio/src/components/TimelineElement.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import WorkIcon from '@mui/icons-material/Work';
-import { borderTop, color } from '@mui/system';
+import {VerticalTimelineElement }  from 'react-vertical-timeline-component';
+
 function TimelineElement(id) {
   return (
     <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: '#fff', color: '#000', borderTop: '7px solid '+id.color }}
+          contentStyle={{ background: '#fff', color: '#000', borderTop: '7px solid '+id.color, boxShadow:'5px 5px 10px grey' }}
           contentArrowStyle={{ borderRight: '7px solid '+id.color }}
           date={id.date}
           iconStyle={{ background: id.color, color: '#fff'}}
           icon={id.icon}
         >
-          <h3 className="vertical-timeline-element-title">{id.title}</h3>
-          <h4 className="vertical-timeline-element-subtitle">{id.place}</h4>
-          <p>
-            {id.explanation}
-          </p>
+          <div className='timelineTextDiv'>
+            <h3 className="vertical-timeline-element-title">{id.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{id.place}</h4>
+            <p>
+              {id.explanation}
+            </p>
+          </div>
+          
         </VerticalTimelineElement>
   )
 }
