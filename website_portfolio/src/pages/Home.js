5e58bff273e041ab67287d431fe81eb7df36d452
HVerschueren <r0842391@student.thomasmore.be>
2023-01-03 14:32:19 +0100

    styling en projecten toegevoegd

--- website_portfolio/src/pages/Home.js
+++ website_portfolio/src/pages/Home.js
@@ -1,108 +1,55 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import WorkIcon from '@mui/icons-material/Work';
-import SchoolIcon from '@mui/icons-material/School';
-import StarIcon from '@mui/icons-material/Star';
 import TimelineElement from '../components/TimelineElement';
 import { TimelineList } from "../helpers/TimelineList"
+import ProjectTile from "../components/ProjectTile"
+import { ProjectList } from '../helpers/ProjectList';
+import '../css/Home.css'
+
+//https://stackoverflow.com/questions/2627650/why-javascript-gettime-is-not-a-function
+// parse a date in yyyy-mm-dd format
+function parseDate(input) {
+  var parts = input.match(/(\d+)/g);
+  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
+//https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
+function _calculateAge(date_of_birth) { // birthday is a date
+  var birthday = parseDate(date_of_birth)
+  
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 function Home() {
   return (
+    <div className='homePage'>
+      <div className='introduction homeDiv'>
+        <p>Welcome on mij portfolio website! My name is <b>Hanne Verschueren</b>, I'm {_calculateAge('1999-12-31')} years old and currently studying Electronics-ICT at Thomase More. Check out my projects or downlaod my cv in the footer.</p>
+      </div>
+
+      <div className='featuredProjects homeDiv'>
+        <h2>featured projects</h2>
+        <div className='projectList'>
+          <ProjectTile id={0} name={ProjectList[0].name} image={ProjectList[0].image} key={0}/>
+          <ProjectTile id={1} name={ProjectList[1].name} image={ProjectList[1].image} key={1}/>
+          <ProjectTile id={2} name={ProjectList[2].name} image={ProjectList[2].image} key={1}/>
+
+      </div>
+      </div>
+
+    
+    <div className='timeline homeDiv'>
+      <h2>timeline</h2>
       <VerticalTimeline lineColor='#13a89e'>
         {TimelineList.map((project, index) => {//gaat door de array in ProjectList en slaat de variablen op in project
           return <TimelineElement id={index} date={project.date} title={project.title} key={index} place={project.place} explanation={project.explanation} icon={project.icon} color={project.color}/>
         })}
-        {/* <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="April 2013"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>
-            Strategy, Social Media
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-          <p>
-            Creative Direction, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          icon={<StarIcon />}
-        /> */}
     </VerticalTimeline>
+    </div>
+    </div>
   )
 }
 
--- website_portfolio/src/pages/ProjectDisplay.js
+++ website_portfolio/src/pages/ProjectDisplay.js
@@ -6,11 +6,15 @@ import "../css/ProjectDisplay.css";
 function ProjectDisplay() {
   const { id } = useParams();
   console.log(id);
-  const project = ProjectList[id];
+  const project =ProjectList[id];
+
   return (
     <div className="project">
       <h1> {project.name}</h1>
-      <img src={project.image} />
+      <div className="imageDiv">
+        <img src={project.image} />
+      </div>
+      
       <h2>my contribution</h2>
       <ul>
         {project.contribution.map((i) => {
